ad32e2eb00105fa687a1d2d9b1c8c1bb43caa0a2
Darren Steverson <dcs194@scarletmail.rutgers.edu>
2019-08-04 14:24:01 -0400

    added deck and displayed user cards, will expand deck with loop

---
+++ assests/game.js
@@ -0,0 +1,13 @@
+console.console.log("Welcome to BlackJack!");
+
+
+var minunmumBet = 10;
+
+var deck = [
+    "Ace of Spades",
+    "Ace of Hearts",
+    "Ace of Clubs",
+    "Ace of Diamonds" 
+];
+
+var userCards = [deck[0], deck[3]];
