d962f380a8bdf92100f6ead57f79b8df0a4f456f
Darren Steverson <dcs194@scarletmail.rutgers.edu>
2019-08-04 15:49:37 -0400

    added functions for new deck, added card object

--- assests/game.js
+++ assests/game.js
@@ -1,13 +1,40 @@
 console.console.log("Welcome to BlackJack!");
 
-
+var suit = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
+var value = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"]
+var deck = newDeck();
 var minunmumBet = 10;
 
-var deck = [
-    "Ace of Spades",
-    "Ace of Hearts",
-    "Ace of Clubs",
-    "Ace of Diamonds" 
-];
+function newDeck (){
+    deck = [];
+        for (i = 0, i < suit.length, i++){
+                (d = 0, d < value.length, d++){
 
-var userCards = [deck[0], deck[3]];
+                    var card {
+                        suit:suit[i],
+                        value:value[d]],
+                    };
+
+                        deck.push(card)
+                        };
+         return deck;
+    };
+};
+
+function cardName(card) {
+    return card.value + " of " + card.suit;
+}
+
+function nextCard (){
+    return deck.shift();
+};
+
+
+var userCards = [nextCard(), nextCard()];
+
+
+
+Alert("Welcome to BlackJack!");
+console.log("Cards Dealt");
+console.log("" + cardName(userCards[0]));
+console.log("" + cardName(userCards[1]));
