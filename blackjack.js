0bbf8975b285c27c2e66cc4c9305bd001cdcf4ef
Darren Steverson <dcs194@scarletmail.rutgers.edu>
2019-08-07 23:46:22 -0400

    added game functionality

--- blackjack.js
+++ blackjack.js
@@ -1,8 +1,13 @@
 
 var suit = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
 var value = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"]
-var deck = newDeck();
-var minunmumBet = 10;
+var startGame = false;
+var gameOver = false;
+var winner = false;
+var deck = [];
+var userCards = [];
+var dealerCards = [];
+var userScore = 0;
 
 // Display to the DOM
 var game = document.getElementById("game");
@@ -13,55 +18,67 @@ var stayButton = document.getElementById("staybutton");
 // Hide Buttons at Start of Game
 hitButton.style.display = "none";
 stayButton.style.display = "none";
+gameStatus();
 
-
-// New Button on onClick
+// New Game Button on onClick
 newGameButton.addEventListener("click", function () {
     game.innerText = "New Game Starting.....";
     newGameButton.style.display = "none";
+    startGame = true;
+    winner = false;
+    gameOver = false;
 
+    // Shuffle New Deck on Button Click
+    deck = newDeck();
+    userCards = [nextCard(), nextCard()];
+    dealerCards = [nextCard(), nextCard()];
+
+    // Hide Buttons After Button Click
     setTimeout(function () {
         hitButton.style.display = "inline";
         stayButton.style.display = "inline";
-        game.innerText = "";
+        gameStatus();
+       
     }, 1500);
-
 });
-
 // New Deck 
 function newDeck() {
     deck = [];
-
     for (i = 0; i < suit.length; i++) {
         for (d = 0; d < value.length; d++) {
-
             var card = {
                 suit: suit[i],
                 value: value[d],
             };
-
             deck.push(card)
         };
         return deck;
     };
 };
-
-
 // Card Name
 function cardName(card) {
     return card.value + " of " + card.suit;
 }
-
 // Next Card 
 function nextCard() {
     return deck.shift();
 };
 
-// Player Cards
-var userCards = [nextCard(), nextCard()];
+// Game Status
+
+function gameStatus(){
+    if (!startGame){
+        title.innerText = "Welcome To BlackJack";
+        game.innerText = "";
+    } else {
+        title.innerText = "";
+        game.innerText = "Your Turn!"
+        };
+};
 
 
-alert("Welcome to BlackJack!");
+
+// Console
 console.log("Welcome to BlackJack!");
 console.log("Cards Dealt");
 console.log("" + cardName(userCards[0]));
